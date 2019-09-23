ea225b400c6c19402d89033950eb551b36707893
Seth Woodworth <swoodworth@cargurus.com>
2019-09-22 20:09:14 -0400

    more lambda, less functions

--- dcc/type_dice.py
+++ dcc/type_dice.py
@@ -1,12 +1,16 @@
 """Dynamically create dice class objects."""
-from random import choice
+from random import choice, choices
 
 
+#: Dungeon Crawl Classics uses the following types of dice:
 DICE = [3, 4, 5, 6, 7, 8, 10, 12, 14, 16, 20, 24, 30]
+
+#: A function to return a random int given a number of die sides
 _roll = lambda s: choice(range(1, s.sides + 1))
-_multiply = lambda s, o: [s() for _ in range(o)]
+#: Allows using the multiply operator to return N * die rolls
+_multiply = lambda s, o: choices(range(1, s.sides + 1), k=o)
 
-
+#: A function to dynamically create
 typecast = lambda sides: type(
     f"d{sides}",
     (object,),
